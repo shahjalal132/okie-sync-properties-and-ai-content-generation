267559fddf9d7ff4179d1b0ea352a4cbf0b52f6c
shahjalal <rjshahjalal132@gmail.com>
2024-11-19 15:15:03 +0600

    added settings tab

---
+++ templates/template-parts/template-part-settings.php
@@ -0,0 +1,8 @@
+<h4 class="common-title">Settings</h4>
+
+<div class="settings-wrapper overflow-hidden">
+    <button type="button" class="save-btn mt-20 button-flex">
+        <span>Fetch Properties</span>
+        <span class="fetch-properties-spinner-loader-wrapper"></span>
+    </button>
+</div>
