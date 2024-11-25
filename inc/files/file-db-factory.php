7c8d3897ae37908db73fe2e7250d1f872df8896e
shahjalal <rjshahjalal132@gmail.com>
2024-11-25 14:32:35 +0600

    removed unused methods

--- inc/files/file-db-factory.php
+++ inc/files/file-db-factory.php
@@ -2,19 +2,6 @@
 
 use Illuminate\Database\Capsule\Manager as DB;
 
-function get_posts_via_query_builder() {
-
-    // Fetch all published posts
-    $posts = DB::table( 'posts' )
-        ->where( 'post_status', 'publish' )
-        ->where( 'post_type', 'post' )
-        ->get();
-
-    foreach ( $posts as $post ) {
-        echo $post->post_title . "\n";
-    }
-}
-
 /**
  * Seed properties to database
  * @param array $properties
